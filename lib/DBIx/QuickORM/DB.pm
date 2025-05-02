2fb4b5cbb91790e59900f3db71826903af5b7546
Chad Granum <exodist7@gmail.com>
2025-05-02 00:20:32 -0700

    Automated Version Bump

--- lib/DBIx/QuickORM/DB.pm
+++ lib/DBIx/QuickORM/DB.pm
@@ -2,7 +2,7 @@ package DBIx::QuickORM::DB;
 use strict;
 use warnings;
 
-our $VERSION = '0.000005';
+our $VERSION = '0.000006';
 
 use Carp qw/croak confess/;
 use Scalar::Util qw/blessed/;
--- lib/DBIx/QuickORM/Role/Type.pm
+++ lib/DBIx/QuickORM/Role/Type.pm
@@ -2,7 +2,7 @@ package DBIx::QuickORM::Role::Type;
 use strict;
 use warnings;
 
-our $VERSION = '0.000005';
+our $VERSION = '0.000006';
 
 use Carp qw/croak/;
 
