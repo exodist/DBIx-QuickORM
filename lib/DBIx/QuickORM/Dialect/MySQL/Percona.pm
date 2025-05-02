16383d1a4256478d45e7aceac124a45ea3ddfc8d
Chad Granum <exodist7@gmail.com>
2025-05-02 01:21:13 -0700

    Automated Version Bump

--- lib/DBIx/QuickORM/Dialect/MySQL/Percona.pm
+++ lib/DBIx/QuickORM/Dialect/MySQL/Percona.pm
@@ -2,7 +2,7 @@ package DBIx::QuickORM::Dialect::MySQL::Percona;
 use strict;
 use warnings;
 
-our $VERSION = '0.000008';
+our $VERSION = '0.000009';
 
 use Carp qw/croak/;
 
--- lib/DBIx/QuickORM/Schema.pm
+++ lib/DBIx/QuickORM/Schema.pm
@@ -2,7 +2,7 @@ package DBIx::QuickORM::Schema;
 use strict;
 use warnings;
 
-our $VERSION = '0.000008';
+our $VERSION = '0.000009';
 
 use Carp qw/confess croak/;
 use Scalar::Util qw/blessed/;
