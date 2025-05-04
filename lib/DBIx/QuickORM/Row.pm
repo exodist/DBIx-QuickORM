fb4cb80b6480e668abcfad68a8b3c5380b7a3ffb
Chad Granum <exodist7@gmail.com>
2025-05-04 01:39:05 -0700

    Make a row role

--- lib/DBIx/QuickORM/Row.pm
+++ lib/DBIx/QuickORM/Row.pm
@@ -22,14 +22,15 @@ use DBIx::QuickORM::Connection::RowData qw{
 
 use DBIx::QuickORM::Util::HashBase qw{
     +row_data
-    +prefetched
 };
+
+use Role::Tiny::With qw/with/;
+with 'DBIx::QuickORM::Role::Row';
 
 sub track_desync { 1 }
 
 sub sqla_source { $_[0]->{+ROW_DATA}->sqla_source }
 sub connection  { $_[0]->{+ROW_DATA}->connection }
-sub dialect     { $_[0]->connection->dialect }
 
 sub row_data { $_[0]->{+ROW_DATA}->active }
 
@@ -45,38 +46,18 @@ sub is_stored   { my $a = $_[0]->{+ROW_DATA}->active(no_fatal => 1); $a && $a->{
 sub is_desynced { my $a = $_[0]->{+ROW_DATA}->active(no_fatal => 1); $a && $a->{+DESYNC}  ? 1 : 0 }
 sub has_pending { my $a = $_[0]->{+ROW_DATA}->active(no_fatal => 1); $a && $a->{+PENDING} ? 1 : 0 }
 
-sub has_field      { $_[0]->sqla_source->has_field($_[1] // croak "Must specify a field name") }
-sub field_affinity { $_[0]->sqla_source->field_affinity($_[1], $_[0]->dialect) }
-
-#<<<
-sub primary_key_field_list { @{$_[0]->sqla_source->primary_key // []} }
-sub primary_key_value_list { map { $_[0]->raw_stored_field($_) // undef } $_[0]->check_pk->primary_key_field_list }
-sub primary_key_hash       { map { $_ => $_[0]->raw_stored_field($_) // undef } $_[0]->check_pk->primary_key_field_list }
-sub primary_key_hashref    { +{ $_[0]->primary_key_hash } }
-#>>>
-
 sub init {
     my $self = shift;
 
     confess "No 'row_data' provided" unless $self->{+ROW_DATA};
 }
 
-sub source {
-    my $self = shift;
-
-    require DBIx::QuickORM::Source;
-    return DBIx::QuickORM::Source->new(
-        sqla_source => $self->sqla_source,
-        connection  => $self->connection,
-    );
-}
-
 sub clone {
-    my $self = shift;
+    my $self      = shift;
     my %overrides = @_;
 
     my $row_data = $self->row_data;
-    my $data = +{ %{$row_data->{+STORED} // {}}, %{$row_data->{+PENDING} // {}} };
+    my $data     = +{%{$row_data->{+STORED} // {}}, %{$row_data->{+PENDING} // {}}};
 
     # Remove primary key fields
     delete $data->{$_} for $self->primary_key_field_list;
@@ -85,7 +66,7 @@ sub clone {
     $data = dclone($data);
 
     # Add in any overrides
-    %$data = ( %$data, %overrides );
+    %$data = (%$data, %overrides);
 
     return $self->source->vivify($data);
 }
@@ -93,12 +74,6 @@ sub clone {
 #####################
 # {{{ Sanity Checks #
 #####################
-
-sub check_pk {
-    return $_[0] if $_[0]->sqla_source->primary_key;
-
-    croak "Operation not allowed: the table this row is from does not have a primary key";
-}
 
 sub check_sync {
     return $_[0] unless $_[0]->track_desync;
@@ -144,42 +119,6 @@ sub force_sync {
     return $self;
 }
 
-sub insert_or_save {
-    my $self = shift;
-
-    return $self->save(@_)   if $self->is_stored;
-    return $self->insert(@_) if $self->has_pending;
-}
-
-sub insert {
-    my $self = shift;
-
-    croak "This row is already in the database" if $self->is_stored;
-    croak "This row has no data to write" unless $self->has_pending;
-
-    $self->connection->insert($self->sqla_source, $self);
-
-    return $self;
-}
-
-sub save {
-    my $self = shift;
-    my %params = @_;
-
-    $self->check_pk;
-    $self->check_sync;
-
-    croak "This row is not in the database yet" unless $self->is_stored;
-
-    my $pk = $self->sqla_source->primary_key or croak "Cannot use 'save()' on a row with a source that has no primary key";
-
-    return $self unless $self->has_pending;
-
-    $self->connection->update($self->sqla_source, $self);
-
-    return $self;
-}
-
 # Fetch new data from the db
 sub refresh {
     my $self = shift;
@@ -216,14 +155,6 @@ sub update {
     $self->save(%params);
 }
 
-sub delete {
-    my $self = shift;
-
-    $self->check_pk;
-
-    $self->connection->delete($self->sqla_source, $self);
-}
-
 ############################
 # }}} Manipulation Methods #
 ############################
@@ -238,8 +169,8 @@ sub raw_field { shift->_field(_raw_field      => @_) }
 sub fields     { my $d = $_[0]->row_data; $_[0]->_fields(_field     => $d->{+PENDING}, $d->{+STORED}) }
 sub raw_fields { my $d = $_[0]->row_data; $_[0]->_fields(_raw_field => $d->{+PENDING}, $d->{+STORED}) }
 
-sub stored_field      { $_[0]->_inflated_field($_[0]->row_data->{+STORED},  $_[1]) }
-sub pending_field     { $_[0]->_inflated_field($_[0]->row_data->{+PENDING}, $_[1]) }
+sub stored_field  { $_[0]->_inflated_field($_[0]->row_data->{+STORED},  $_[1]) }
+sub pending_field { $_[0]->_inflated_field($_[0]->row_data->{+PENDING}, $_[1]) }
 
 sub raw_stored_field  { $_[0]->_raw_field($_[0]->row_data->{+STORED},  $_[1]) }
 sub raw_pending_field { $_[0]->_raw_field($_[0]->row_data->{+PENDING}, $_[1]) }
@@ -260,8 +191,8 @@ sub field_is_desynced {
 }
 
 sub _field {
-    my $self = shift;
-    my $meth = shift;
+    my $self  = shift;
+    my $meth  = shift;
     my $field = shift or croak "Must specify a field name";
 
     croak "This row does not have a '$field' field" unless $self->has_field($field);
@@ -269,7 +200,7 @@ sub _field {
     my $row_data = $self->row_data;
 
     if (@_) {
-        $self->check_pk if $row_data->{+STORED}; # We can set a field if the row has not been inserted yet, or if it has a pk
+        $self->check_pk if $row_data->{+STORED};    # We can set a field if the row has not been inserted yet, or if it has a pk
         $row_data->{+PENDING}->{$field} = shift;
     }
 
@@ -350,87 +281,6 @@ sub _raw_field {
 ####################
 # {{{ Link methods #
 ####################
-
-sub follow {
-    my $self = shift;
-    my ($link) = @_;
-    $link = $self->parse_link($link);
-
-    my $where = {};
-    for my $set (zip($link->local_columns, $link->other_columns)) {
-        my ($local, $other) = @$set;
-        $where->{$other} = $self->field($local);
-    }
-
-    return $self->connection->select($link->other_table, $where);
-}
-
-sub obtain {
-    my $self = shift;
-    my ($link) = @_;
-    $link = $self->parse_link($link);
-    croak "The specified link does not point to a unique row" unless $link->unique;
-
-    if (my $prefetch = $self->{+PREFETCHED}) {
-        my $table = $link->other_table;
-        my $key = $link->key;
-        if (my $set = $prefetch->{$table}) {
-            return $set->{$key} if $set->{$key};
-        }
-    }
-
-    $self->follow($link)->one;
-}
-
-sub insert_related {
-    my $self = shift;
-    my ($link, $row_data) = @_;
-    $link = $self->parse_link($link);
-
-    for my $set (zip($link->local_columns, $link->other_columns)) {
-        my ($local, $other) = @$set;
-        croak "field '$other' already exists in provided row data" if exists $row_data->{$other};
-        $row_data->{$other} = $self->field($local);
-    }
-
-    $self->connection->insert($link->other_table() => $row_data);
-}
-
-sub siblings { # This includes the original
-    my $self = shift;
-    my ($link_or_fields) = @_;
-
-    croak "You must specify a link or arrayref of fields to search on" unless $link_or_fields;
-
-    my $fields;
-    if (ref($link_or_fields) eq 'ARRAY') {
-        $fields = $link_or_fields;
-    }
-    else {
-        $fields = $self->parse_link($link_or_fields)->local_columns;
-    }
-
-    my $where = +{ map { $_ => $self->field($_) } @$fields };
-    return $self->source->select($where);
-}
-
-sub parse_link {
-    my $self = shift;
-    my ($link) = @_;
-
-    return $link if blessed($link) && $link->isa('DBIx::QuickORM::Link');
-
-    my $ref = ref($link);
-
-    return $self->sqla_source->links_by_alias->{$link} // croak "'$link' is not a valid link alias for table '" . $self->sqla_source->name . "'"
-        unless $ref;
-
-    return DBIx::QuickORM::Link->parse(
-        sqla_source => $self->sqla_source,
-        connection  => $self->connection,
-        link        => $link,
-    );
-}
 
 ####################
 # }}} Link methods #
