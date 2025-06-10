ba809508ad1c574ca19f9b077d19a2068b2b676f
Chad Granum <exodist7@gmail.com>
2025-06-10 13:21:54 -0700

    Doc updates

--- lib/DBIx/QuickORM/Connection.pm
+++ lib/DBIx/QuickORM/Connection.pm
@@ -969,4 +969,34 @@ These are shortcuts for:
 
 =back
 
+=head1 SOURCE
+
+The source code repository for DBIx-QuickORM can be found at
+L<http://github.com/exodist/DBIx-QuickORM/>.
+
+=head1 MAINTAINERS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright Chad Granum E<lt>exodist7@gmail.comE<gt>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See L<http://dev.perl.org/licenses/>
+
 =cut
--- lib/DBIx/QuickORM/Handle.pm
+++ lib/DBIx/QuickORM/Handle.pm
@@ -2074,3 +2074,35 @@ Run the callback for each row found.
 In data_only mode this will provide hashrefs instead of blessed row objects.
 
 =back
+
+=head1 SOURCE
+
+The source code repository for DBIx-QuickORM can be found at
+L<http://github.com/exodist/DBIx-QuickORM/>.
+
+=head1 MAINTAINERS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 AUTHORS
+
+=over 4
+
+=item Chad Granum E<lt>exodist@cpan.orgE<gt>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright Chad Granum E<lt>exodist7@gmail.comE<gt>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See L<http://dev.perl.org/licenses/>
+
+=cut
